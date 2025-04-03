00001dacf5c1f619bd4af692a1cea04527fcf30c
Susanne714 <sambale.su@gmail.com>
2025-04-03 17:56:49 +0200

    Input, FormField & DatePicker imported

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { MatDialogModule, MatDialogActions, MatDialogContent } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [MatDialogActions, MatDialogContent, MatDialogModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [MatDialogActions, MatDialogContent, MatDialogModule, MatInputModule, MatFormFieldModule, MatButtonModule, MatDatepickerModule],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
