f2e4555b113f6e5515edda6e69dbb9d934811704
Susanne714 <sambale.su@gmail.com>
2025-04-04 12:49:34 +0200

    FormsModule added to use ngModel

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatDialogModule, MatDialogActions, MatDialogContent } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from '@angular/material/core';
+import { User } from '../../models/user.class';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
   providers: [provideNativeDateAdapter()],
-  imports: [MatDialogActions, MatDialogContent, MatDialogModule, MatInputModule, MatFormFieldModule, MatButtonModule, MatDatepickerModule],
+  imports: [FormsModule, MatDialogActions, MatDialogContent, MatDialogModule, MatInputModule, MatFormFieldModule, MatButtonModule, MatDatepickerModule],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
 export class DialogAddUserComponent {
+  user = new User();
+  birthDate: Date = new Date();
+
+  saveUser() {
+    this.user.birthDate.toLocaleString();
+    console.log('current user is', this.user)
+  }
 
 }
